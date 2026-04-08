cd67be10a0396ba22b08c05af6f5801e9da4ea83
Vaidehi2355 <vjadhav2355@gmail.com>
2026-04-08 23:36:27 +0530

    fixed port binding

--- app.py
+++ app.py
@@ -652,4 +652,4 @@ def resume_analyser():
 # ─── Run ──────────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(debug=True , port=5000)
+    app.run()
